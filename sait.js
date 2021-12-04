9f07381ec3edea66b135da923bd686211bd9ebd3
jake9412 <ifuga95@mail.ru>
2021-12-05 02:51:41 +0300

    result

---
+++ sait.js
@@ -0,0 +1,16 @@
+function sm(){
+    alert("inst - @ecowarehousenz     facebook - @EcoWarehouseNZ ");
+}
+function prod(){
+    window.scrollTo({
+        top:10000,
+        behavior:"smooth",
+    })
+}
+
+function verh(){
+    window.scrollTo({
+        top:100,
+        behavior:"smooth",
+    })
+}
